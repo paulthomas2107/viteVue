5d1b28fc5259f311bd85544ad97c73bb4d2c5b15
Paul Thomas <paulthomas2107@gmail.com>
2021-05-14 19:29:04 +0100

    Init...

---
+++ src/components/MyForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <p>{{ name }}</p>
+    <input ref="el" type="text" placeholder="Name" v-model="name" />
+    <div>
+      <button @click="submitForm" v-bind:disabled="name.length == 0">
+        Submit
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+export default {
+  setup() {
+    const name = ref("");
+    const el = ref();
+    const submitForm = () => {
+      console.log(`Form submitted name = ${name.value}`);
+    };
+
+    console.log(el.value);
+
+    onMounted(() => {
+      el.value.focus();
+    });
+    return {
+      name,
+      submitForm,
+      el,
+    };
+  },
+};
+</script>
+
+<style>
+input[type="text"] {
+  padding: 5px;
+}
+button {
+  margin-top: 20px;
+}
+</style>
